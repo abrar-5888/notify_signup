25ec2ad9df7dd9a902b901b847518d169ca60180
abrar-5888 <bsem-f20-003@superior.edu.pk>
2023-10-10 15:47:49 +0500

    main

---
+++ lib/Model/NewUsers.dart
@@ -0,0 +1,27 @@
+class RequestUsers {
+  String name;
+  String email;
+  String password;
+  String phoneNo;
+  String designation;
+  String age;
+  String owner;
+  String address;
+  String fname;
+  String fphoneNo;
+  String uid;
+
+  RequestUsers({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.phoneNo,
+    required this.designation,
+    required this.age,
+    required this.owner,
+    required this.address,
+    required this.fname,
+    required this.fphoneNo,
+    required this.uid,
+  });
+}
